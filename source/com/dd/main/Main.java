08ce71d558e2d2dfe66cfef13c1da82fdd30c40d
SuBpEx <Moritz-0399@web.de>
2017-03-16 18:07:35 +0100

    Moritz kann auch sachen machen

--- source/com/dd/main/Main.java
+++ source/com/dd/main/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
     private static String playerName;
     private Scanner in = new Scanner(System.in);
-
-    //Moritz kann stuff schreiben
+    
     public static void main(String[] args) {
         Main m = new Main();
         m.initializeGame();
